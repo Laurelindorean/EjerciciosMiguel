29f6bc77c455274928eef11a3f01788cdcf718be
Laurelindorean <kguinera@hotmail.com>
2022-09-04 21:46:35 +0200

    initial commit

---
+++ src/miPackage/MiClase.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package miPackage;
+
+/**
+ * @author Palmira
+ *
+ */
+public class MiClase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello git!");
+	}
+
+}
