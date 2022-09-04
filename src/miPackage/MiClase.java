d24813733dd74cb1fb46c97ed6b510d72ce11729
Laurelindorean <kguinera@hotmail.com>
2022-09-04 21:50:56 +0200

    nuevo cambio

--- src/miPackage/MiClase.java
+++ src/miPackage/MiClase.java
@@ -14,7 +14,8 @@ public class MiClase {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello git!");
+		System.out.println("Hello git!");	
+		// Probando una cosita
 	}
 
 }
